e40a75cc86842e93d0f36a2a3d8ff9f2deed1c6a
reach2nimit <reach2nimit@gmail.com>
2026-08-12 12:32:54 +0530

    Time: 3 ms (21.95%), Space: 43.6 MB (46.18%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        int totalCost = 0;
+        int n = costs.length/2;
+        Arrays.sort(costs, (a,b) ->{
+            return (a[0] - a[1]) - (b[0] - b[1]);
+        });
+
+        for(int i = 0; i < n; i++){
+            totalCost += costs[i][0];
+            totalCost += costs[n+i][1];
+        }
+        return totalCost;
+    }
+}
