3716011959c822726b2ee382e8ae1fd76f0a9743
reach2nimit <reach2nimit@gmail.com>
2026-08-17 13:10:13 +0530

    Time: 8 ms (30.23%), Space: 44.3 MB (37.11%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+       // 100 64 25 9 4
+       // 64 25 10 9 4
+       //25 10 9 8 4
+       //10 9 8 5 4
+       // 9 8 5 4 3
+
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int val : gifts)
+            pq.offer(val);
+        
+        while(k > 0){
+            int val = pq.remove();
+            val = (int)Math.floor(Math.sqrt(val));
+            pq.offer(val);
+            k--;
+        }
+
+        long sum = 0;
+        while(!pq.isEmpty()){
+            sum+=(pq.remove());
+        }
+
+        return sum;
+    }
+}
